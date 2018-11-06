c09da6fc296c61008f5f3f27a5de64bbd8343680
kiliw <kilocheat@gmail.com>
2018-11-06 08:53:47 +0100

    Add clock-mirror-challenge.

--- src/challenge.test.ts
+++ src/challenge.test.ts
@@ -1,7 +1,20 @@
 import test from 'tape'
-import { add } from './challenge'
+import { WhatIsTheTime } from './challenge'
 
-test('Should add two numbers', assert => {
+test('Basic Test', assert => {
   assert.plan(1)
-  assert.equal(add(1, 2), 3)
+  assert.equal(WhatIsTheTime('06:35'), '05:25')
+})
+
+test('Basic Test', assert => {
+  assert.plan(1)
+  assert.equal(WhatIsTheTime('11:59'), '12:01')
+})
+test('Basic Test', assert => {
+  assert.plan(1)
+  assert.equal(WhatIsTheTime('12:02'), '11:58')
+})
+test('Basic Test', assert => {
+  assert.plan(1)
+  assert.equal(WhatIsTheTime('06:00'), '06:00')
 })
--- src/challenge.ts
+++ src/challenge.ts
@@ -1,3 +1,5 @@
-import R from 'ramda'
-
-export const add: (a: number, b: number) => number = R.add
+// Taken from https://www.codewars.com/kata/clock-in-mirror/javascript
+export function WhatIsTheTime(timeInMirror: string) {
+  // "05:25" --> "06:35";
+  // Happy Coding
+}
