8e32af0b583588df3d445898593f6ed0ede8f6a0
Levin Keller <post@levinkeller.de>
2018-11-06 15:08:18 +0100

    Add "simple encryption challenge" https://www.codewars.com/kata/57814d79a56c88e3e0000786/train/javascript

---
+++ src/simpleEncryption.js
@@ -0,0 +1,12 @@
+//Taken from https://www.codewars.com/kata/simple-encryption-number-1-alternating-split/javascript
+function encrypt(text, n) {
+  
+}
+
+function decrypt(encryptedText, n) {
+}
+
+module.exports = {
+  decrypt,
+  encrypt,
+}
---
+++ src/simpleEncryption.test.js
@@ -0,0 +1,52 @@
+const test = require('tape')
+const { decrypt, encrypt } = require('./simpleEncryption')
+
+test('Solution', function(t) {
+  t.test('EncryptExampleTests', function(assert) {
+    assert.equals(encrypt('This is a test!', 0), 'This is a test!')
+    assert.equals(encrypt('This is a test!', 1), 'hsi  etTi sats!')
+    assert.equals(encrypt('This is a test!', 2), 's eT ashi tist!')
+    assert.equals(encrypt('This is a test!', 3), ' Tah itse sits!')
+    assert.equals(encrypt('This is a test!', 4), 'This is a test!')
+    assert.equals(encrypt('This is a test!', -1), 'This is a test!')
+    assert.equals(
+      encrypt('This kata is very interesting!', 1),
+      'hskt svr neetn!Ti aai eyitrsig'
+    )
+    assert.end()
+  })
+})
+
+test('Solution', function(t) {
+  t.test('DecryptExampleTests', function(assert) {
+    assert.equals(decrypt('This is a test!', 0), 'This is a test!')
+    assert.equals(
+      decrypt('hsi  etTi sats!', 1),
+      'This is a test!',
+      '1 step decryption'
+    )
+    assert.equals(
+      decrypt('s eT ashi tist!', 2),
+      'This is a test!',
+      'Two step decryption'
+    )
+    assert.equals(decrypt(' Tah itse sits!', 3), 'This is a test!')
+    assert.equals(decrypt('This is a test!', 4), 'This is a test!')
+    assert.equals(decrypt('This is a test!', -1), 'This is a test!')
+    assert.equals(
+      decrypt('hskt svr neetn!Ti aai eyitrsig', 1),
+      'This kata is very interesting!'
+    )
+    assert.end()
+  })
+})
+
+test('Solution', function(t) {
+  t.test('Null or Empty', function(assert) {
+    assert.equals(encrypt('', 0), '')
+    assert.equals(decrypt('', 0), '')
+    assert.equals(encrypt(null, 0), null)
+    assert.equals(decrypt(null, 0), null)
+    assert.end()
+  })
+})
