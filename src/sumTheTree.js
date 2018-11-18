e57a11b55ffd22697e28560f07b87bf239d9159e
Levin Keller <post@levinkeller.de>
2018-11-18 07:47:27 +0100

    Remove all solutions.

--- src/sumTheTree.js
+++ src/sumTheTree.js
@@ -1,9 +1,5 @@
-// return the sum of all values in the tree, including the root
+// Taken from https://www.codewars.com/kata/sum-the-tree/javascript
 function sumTheTreeValues(root) {
-  if (root === null) {
-    return 0
-  }
-  return root.value + sumTheTreeValues(root.left) + sumTheTreeValues(root.right)
 }
 
 module.exports = { sumTheTreeValues }
