0328d47ba18527d74d4afcfbd42ca4bcf8b1454e
kiliw <kilocheat@gmail.com>
2018-11-19 12:55:43 +0100

    Add clock mirror solution.

--- src/clockMirror.js
+++ src/clockMirror.js
@@ -2,6 +2,27 @@
 function WhatIsTheTime(timeInMirror) {
   //"05:25" --> "06:35";
   // Happy Coding
+  const hour = parseInt(timeInMirror.split(':')[0]) % 12
+  const minute = parseInt(timeInMirror.split(':')[1])
+
+  const resultInMinutes = 720 - (hour * 60 + minute)
+
+  let hours = Math.floor(resultInMinutes / 60)
+  let minutes = Math.floor(resultInMinutes % 60)
+
+  if (hours === 0) {
+    hours = 12
+  }
+
+  if (hours.toString().length === 1) {
+    hours = `0${hours}`
+  }
+
+  if (minutes.toString().length === 1) {
+    minutes = `0${minutes}`
+  }
+
+  return `${hours}:${minutes}`
 }
 
 module.exports = { WhatIsTheTime }
