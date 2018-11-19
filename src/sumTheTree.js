b5b2fc49fd37307fe6c04d0b45e19e306e808647
kiliw <kilocheat@gmail.com>
2018-11-19 11:51:07 +0100

    Add solution for sumTheTree

--- src/sumTheTree.js
+++ src/sumTheTree.js
@@ -1,5 +1,9 @@
 // Taken from https://www.codewars.com/kata/sum-the-tree/javascript
 function sumTheTreeValues(root) {
+  if (root === null) {
+    return 0
+  }
+  return root.value + sumTheTreeValues(root.left) + sumTheTreeValues(root.right)
 }
 
 module.exports = { sumTheTreeValues }
