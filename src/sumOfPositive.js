90e9db2f9d39556e6e2a4c8acb30c7ce65457079
kiliw <kilocheat@gmail.com>
2018-11-19 11:03:55 +0100

    Add solution for sum-of-positive challenge

--- src/sumOfPositive.js
+++ src/sumOfPositive.js
@@ -1,5 +1,13 @@
 //https://www.codewars.com/kata/sum-of-positive/javascript
+const R = require('ramda')
 
-const sumOfPositive = (array) => {}
+const secondArgIsPositive = R.pipe(
+  R.nthArg(1),
+  R.gt(R.__, 0)
+)
+
+const addSecondArgIffPositive = R.ifElse(secondArgIsPositive, R.add, R.identity)
+
+const sumOfPositive = R.reduce(addSecondArgIffPositive, 0)
 
 module.exports = { sumOfPositive }
