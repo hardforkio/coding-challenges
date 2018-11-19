19baa064ad05121016f3366e68dbf2018d635858
kiliw <kilocheat@gmail.com>
2018-11-19 11:31:00 +0100

    Add mumbling challenge.

---
+++ src/mumbling.js
@@ -0,0 +1,5 @@
+//https://www.codewars.com/kata/mumbling/javascript
+
+function accum(string) {}
+
+module.exports = { accum }
---
+++ src/mumbling.test.js
@@ -0,0 +1,19 @@
+const test = require('tape')
+const { accum } = require('./mumbling')
+
+test.only('Simple Test', function(t) {
+  t.test('1', function(assert) {
+    assert.plan(1)
+    assert.equal(
+      accum('ZpglnRxqenU'),
+      'Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu'
+    )
+  })
+  t.test('2', function(assert) {
+    assert.plan(1)
+    assert.equal(
+      accum('NyffsGeyylB'),
+      'N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb'
+    )
+  })
+})
