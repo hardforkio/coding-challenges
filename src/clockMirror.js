00b60659d8470e3fdae0c8bfda27fb014ea87f27
Levin Keller <post@levinkeller.de>
2018-11-27 08:18:52 +0100

    Update challenge to typescript.

--- src/clockMirror.js
+++
@@ -1,7 +0,0 @@
-//Taken from https://www.codewars.com/kata/clock-in-mirror/javascript
-function WhatIsTheTime(timeInMirror) {
-  //"05:25" --> "06:35";
-  // Happy Coding
-}
-
-module.exports = { WhatIsTheTime }
