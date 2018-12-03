9bfa3c518fbfa81e033e2ae34879f54f7cfe2dc4
kiliw <kilocheat@gmail.com>
2018-12-03 11:05:59 +0100

    Add deep equal challenge.

---
+++ src/deep-equal.ts
@@ -0,0 +1,7 @@
+// implement a deep equal comparision with ramda between two arrays
+
+import * as R from "ramda"
+
+export const deepEqual = (array1: number[], array2: number[]) => {
+  return 0
+}
