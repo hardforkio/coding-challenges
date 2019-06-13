e6b114cd399cbd2e75ce90b5b6bbfa03dd6a5223
kolja <kolja@Florians-MacBook-Pro-4.local>
2019-06-13 11:51:16 +0200

    add deep equal function

--- src/deep-equal.ts
+++ src/deep-equal.ts
@@ -1,5 +1,15 @@
 import * as R from "ramda"
 
 export const deepEqual = (array1: number[], array2: number[]) => {
+ if(array1.length!=array2.length){
   return false
+ }
+  for (let i = 0; i < array1.length; i++) {
+  if(array1[i]!= array2[i]){
+  return false
+  }
+ }
+  
+  return true
 }
+
