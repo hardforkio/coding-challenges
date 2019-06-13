82577f3b73e53741e541268ce17af15e64941c2c
kolja <kolja@Florians-MacBook-Pro-4.local>
2019-06-13 12:55:58 +0200

    implement funnction

--- src/sumOfPositive.js
+++ src/sumOfPositive.js
@@ -1,5 +1,13 @@
 //https://www.codewars.com/kata/sum-of-positive/javascript
 
-const sumOfPositive = (array) => {}
+const sumOfPositive = (array) => {
+let result = 0
+for (let i = 0; i < array.length; i++) {
+    if(array[i] > 0) {
+        result = result + array[i]
+    }
+}
+return result
+}
 
 module.exports = { sumOfPositive }
