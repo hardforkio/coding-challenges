f677fe1cdd5c8e2015b89d101c9e4e2b4b4f79ac
Cavan King <king@hardfork.io>
2019-07-16 10:56:23 +0200

    Approach using iterator
    2 Functions:
     - One generates iterator that walks tree
     - One consumes iterator values and sums them

--- src/sumTheTree.js
+++ src/sumTheTree.js
@@ -1,5 +1,25 @@
 // Taken from https://www.codewars.com/kata/sum-the-tree/javascript
-function sumTheTreeValues(root) {
+/**
+ * Generates an iterator to a walk binary tree in pre-order
+ */
+function* preorderWalk(root) {
+    if (root.left)
+        yield* preorderWalk(root.left);
+    yield root;
+    if (root.right)
+        yield* preorderWalk(root.right);
 }
 
-module.exports = { sumTheTreeValues }
+/**
+ * Sum up values in a binary tree
+ */
+function sumTheTreeValues(root) {
+    //generate iterator
+    //sum up values (reduce with +)
+    var sum = 0;
+    for (node of preorderWalk(root))    //Is there a way to use map() and reduce()?
+        sum += node.value;
+    return sum;
+}
+
+module.exports = { sumTheTreeValues };
