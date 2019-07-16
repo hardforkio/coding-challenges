1b0bb95dff76f25b98c5cb6b0960d35593253aae
Cavan King <king@hardfork.io>
2019-07-16 10:21:04 +0200

    Straightforward recursive approach
    Problem: may not be tail recursive when tree is a leftward chain

--- src/sumTheTree.js
+++ src/sumTheTree.js
@@ -1,5 +1,15 @@
 // Taken from https://www.codewars.com/kata/sum-the-tree/javascript
+/**
+ * Sums up values in binary tree
+ * * Assumptions:
+ *  - null is not a valid tree
+ *  - the function will only receive valid trees as input
+ * @param {*} root 
+ */
 function sumTheTreeValues(root) {
+    return (root.left ? sumTheTreeValues(root.left) : 0)
+        + root.value
+        + (root.right ? sumTheTreeValues(root.right) : 0);
 }
 
-module.exports = { sumTheTreeValues }
+module.exports = { sumTheTreeValues };
