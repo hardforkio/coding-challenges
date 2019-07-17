a6c70e576b3114789ebceefd818a41c3e2cbc646
Cavan King <king@hardfork.io>
2019-07-17 09:09:23 +0200

    Solution with filter/reduce and named fns

--- src/sumOfPositive.js
+++ src/sumOfPositive.js
@@ -1,5 +1,7 @@
 //https://www.codewars.com/kata/sum-of-positive/javascript
 
-const sumOfPositive = (array) => {}
+const isPositiveNumber = (n) => n > 0
+const sum = (acc, x) => acc + x
+const sumOfPositive = (array) => array.filter(isPositiveNumber).reduce(sum, 0)
 
 module.exports = { sumOfPositive }
