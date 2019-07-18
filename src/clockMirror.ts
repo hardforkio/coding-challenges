fa633cd6b3f6ecab4ddf6d9c0c5580177577c726
Cavan King <king@hardfork.io>
2019-07-18 11:29:32 +0200

    WIP (moved helpers to submodule)
    Todo next - tests

--- src/clockMirror.ts
+++ src/clockMirror.ts
@@ -1,39 +1,22 @@
-const R = require("ramda");
-const util = require("util");
-
 // Taken from https://www.codewars.com/kata/clock-in-mirror/javascript
+const R = require("ramda");
+const {
+  timestampToMinutes,
+  minutesToTimestamp,
+  strangeTimetoModulo720Time,
+  modulo720TimeToStrangeTime,
+  weirdCoordinteSystemOriginToGoodCoordinateSystemOrigin,
+  goodCoordinteSystemOriginToWeirdCoordinateSystemOrigin
+} = require("./clockMirrorHelpers");
 
-const timestampToMinutes = (stringTime: string) => {
-  const h = parseInt(stringTime.substring(0, 3));
-  const m = parseInt(stringTime.substring(4, 6));
-  return 60 * h + m;
-};
-
-const minutesToTimestamp = (minutes: number) => {
-  const padTo2Digits = (n: number) => n.toString().padStart(2);
-  const h = minutes / 60;
-  const m = minutes % 60;
-  return `${padTo2Digits(h)}:${padTo2Digits(m)}`;
-};
-
-const strangeTimetoModulo720Time = (t: number) => t - 60;
-const modulo720TimeToStrangeTime = (t: number) => t + 60;
-
-//todo more descriptive name
-const weirdCoordinteSystemOriginToGoodCoordinateSystemOrigin = (t: number) =>
-  ((t - 11 * 60) % 12) * 60;
-const GoodCoordinteSystemOriginToWeirdCoordinateSystemOrigin = (t: number) =>
-  ((t + 11 * 60) % 12) * 60;
-
-//keep this name, however
-const crazyTimeNotationToSaneTimeNotation = R.pipe(
+export const crazyTimeNotationToSaneTimeNotation = R.pipe(
   timestampToMinutes,
   strangeTimetoModulo720Time,
   weirdCoordinteSystemOriginToGoodCoordinateSystemOrigin
 );
 
-const saneTimeNotationToCrazyTimeNotation = R.pipe(
-  GoodCoordinteSystemOriginToWeirdCoordinateSystemOrigin,
+export const saneTimeNotationToCrazyTimeNotation = R.pipe(
+  goodCoordinteSystemOriginToWeirdCoordinateSystemOrigin,
   modulo720TimeToStrangeTime,
   minutesToTimestamp
 );
