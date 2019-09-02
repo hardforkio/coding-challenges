13b9ba8b08d88131211bca3c4b3997134254c04b
EmCeeEs <ab22uhrnachtunruhe@gmail.com>
2019-09-02 09:27:09 +0200

    functional solution using ramda

--- src/sumOfPositive.js
+++ src/sumOfPositive.js
@@ -1,5 +1,9 @@
 //https://www.codewars.com/kata/sum-of-positive/javascript
+const R = require('ramda')
 
-const sumOfPositive = (array) => {}
+const isPositive = n => n > 0
+
+const sum = R.reduce(R.add, 0)
+const sumOfPositive = R.pipe(R.filter(isPositive), sum)
 
 module.exports = { sumOfPositive }
