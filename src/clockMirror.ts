87362db2794209bad910609202d72e521f5d924f
EmCeeEs <ab22uhrnachtunruhe@gmail.com>
2019-09-11 11:15:38 +0200

    Add and configure prettier.

--- src/clockMirror.ts
+++ src/clockMirror.ts
@@ -1,5 +1,6 @@
 // Taken from https://www.codewars.com/kata/clock-in-mirror/javascript
+
 export function WhatIsTheTime(timeInMirror: string) {
-  // "05:25" --> "06:35";
-  // Happy Coding
+    // "05:25" --> "06:35";
+    // Happy Coding
 }
