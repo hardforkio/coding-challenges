2d5f14cfed06883dee481495c34eaa2db486f2e1
EmCeeEs <ab22uhrnachtunruhe@gmail.com>
2019-09-13 13:11:29 +0200

    Add solution to challenge/clock-mirror

--- src/clockMirror.test.ts
+++ src/clockMirror.test.ts
@@ -1,19 +1,28 @@
 import test from "tape"
-import { WhatIsTheTime } from "./clockMirror"
+import { whatIsTheTime } from "./clockMirror"
+import { toTotalMinutes } from "./clockMirror"
 
 test("Basic Test", (assert) => {
     assert.plan(1)
-    assert.equal(WhatIsTheTime("06:35"), "05:25")
+    assert.equal(whatIsTheTime("06:35"), "05:25")
 })
 test("Basic Test", (assert) => {
     assert.plan(1)
-    assert.equal(WhatIsTheTime("11:59"), "12:01")
+    assert.equal(whatIsTheTime("11:59"), "12:01")
 })
 test("Basic Test", (assert) => {
     assert.plan(1)
-    assert.equal(WhatIsTheTime("12:02"), "11:58")
+    assert.equal(whatIsTheTime("12:02"), "11:58")
 })
 test("Basic Test", (assert) => {
     assert.plan(1)
-    assert.equal(WhatIsTheTime("06:00"), "06:00")
+    assert.equal(whatIsTheTime("06:00"), "06:00")
+})
+
+test("Total Minutes", (assert) => {
+    assert.equal(toTotalMinutes("00:00"), 0)
+    assert.equal(toTotalMinutes("12:00"), 0)
+    assert.equal(toTotalMinutes("1:59"), 119)
+    assert.equal(toTotalMinutes("11:01"), 661)
+    assert.end()
 })
--- src/clockMirror.ts
+++ src/clockMirror.ts
@@ -1,6 +1,54 @@
 // Taken from https://www.codewars.com/kata/clock-in-mirror/javascript
+import R from "ramda"
 
-export function WhatIsTheTime(timeInMirror: string) {
-    // "05:25" --> "06:35";
-    // Happy Coding
-}
+const parseHours = R.pipe(
+    R.head,
+    parseInt,
+    R.cond<number, number>([[R.equals(12), R.always(0)], [R.T, R.identity]]),
+    R.multiply(60),
+)
+
+const parseMinutes = R.pipe(
+    R.tail,
+    parseInt,
+)
+
+export const toTotalMinutes = R.pipe(
+    R.split(":"),
+    R.converge(R.add, [parseHours, parseMinutes]),
+)
+
+const doMirrorTime = R.subtract(12 * 60)
+
+const asTwoCharacterStr = R.pipe(
+    R.toString,
+    R.concat("0"),
+)
+
+const toHourString = R.pipe(
+    R.divide(R.__, 60),
+    Math.floor,
+    R.cond<number, string>([
+        [R.equals(0), R.always("12")],
+        [R.lt(R.__, 10), asTwoCharacterStr],
+        [R.T, R.toString],
+    ]),
+)
+
+const toMinutesString = R.pipe(
+    R.modulo(R.__, 60),
+    R.cond<number, string>([
+        [R.lt(R.__, 10), asTwoCharacterStr],
+        [R.T, R.toString],
+    ]),
+)
+
+const customJoin = (x: string, y: string) => R.join(":", [x, y])
+
+const toClockString = R.converge(customJoin, [toHourString, toMinutesString])
+
+export const whatIsTheTime = R.pipe(
+    toTotalMinutes,
+    doMirrorTime,
+    toClockString,
+)
