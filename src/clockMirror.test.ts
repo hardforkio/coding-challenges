25c09cc4702ac45a3d741e2f2ae0cdd48f7aac4e
EmCeeEs <ab22uhrnachtunruhe@gmail.com>
2019-09-16 08:27:52 +0200

    Add maybe functor to solution.

--- src/clockMirror.test.ts
+++ src/clockMirror.test.ts
@@ -1,28 +1,28 @@
 import test from "tape"
 import { whatIsTheTime } from "./clockMirror"
-import { toTotalMinutes } from "./clockMirror"
+import { toClockString, toTotalMinutes } from "./clockMirror"
 
 test("Basic Test", (assert) => {
-    assert.plan(1)
+    assert.plan(4)
     assert.equal(whatIsTheTime("06:35"), "05:25")
-})
-test("Basic Test", (assert) => {
-    assert.plan(1)
     assert.equal(whatIsTheTime("11:59"), "12:01")
-})
-test("Basic Test", (assert) => {
-    assert.plan(1)
     assert.equal(whatIsTheTime("12:02"), "11:58")
-})
-test("Basic Test", (assert) => {
-    assert.plan(1)
     assert.equal(whatIsTheTime("06:00"), "06:00")
 })
 
 test("Total Minutes", (assert) => {
     assert.equal(toTotalMinutes("00:00"), 0)
     assert.equal(toTotalMinutes("12:00"), 0)
-    assert.equal(toTotalMinutes("1:59"), 119)
-    assert.equal(toTotalMinutes("11:01"), 661)
+    assert.equal(toTotalMinutes("11:59"), 12 * 60 - 1)
+    assert.equal(toTotalMinutes("01:59"), 119)
+    assert.end()
+})
+
+test("Clock String", (assert) => {
+    assert.equal(toClockString(0), "12:00")
+    assert.equal(toClockString(12 * 60), "12:00")
+    assert.equal(toClockString(60), "01:00")
+    assert.equal(toClockString(61), "01:01")
+    assert.equal(toClockString(5), "12:05")
     assert.end()
 })
--- src/clockMirror.ts
+++ src/clockMirror.ts
@@ -1,10 +1,22 @@
 // Taken from https://www.codewars.com/kata/clock-in-mirror/javascript
 import R from "ramda"
 
+import * as M from "ts.data.maybe"
+
+const map = R.curry(M.map)
+const maybe = R.curry((defaultValue: string, instance: M.Maybe<string>) =>
+    M.withDefault(instance, defaultValue),
+)
+
+const validateInputString = R.ifElse(R.test(/^\d\d:\d\d$/), M.just, M.nothing)
+
+const highNoon2Noon = R.ifElse(R.equals(12), R.always(0), R.identity)
+const noon2HighNoon = R.ifElse(R.equals(0), R.always(12), R.identity)
+
 const parseHours = R.pipe(
     R.head,
     parseInt,
-    R.cond<number, number>([[R.equals(12), R.always(0)], [R.T, R.identity]]),
+    highNoon2Noon,
     R.multiply(60),
 )
 
@@ -18,37 +30,42 @@ export const toTotalMinutes = R.pipe(
     R.converge(R.add, [parseHours, parseMinutes]),
 )
 
-const doMirrorTime = R.subtract(12 * 60)
+const doMirroring = R.subtract(12 * 60)
 
-const asTwoCharacterStr = R.pipe(
+const asTwoCharacterString = R.pipe(
     R.toString,
     R.concat("0"),
+)
+
+const formatAsString = R.ifElse(
+    R.lt(R.__, 10),
+    asTwoCharacterString,
+    R.toString,
 )
 
 const toHourString = R.pipe(
     R.divide(R.__, 60),
     Math.floor,
-    R.cond<number, string>([
-        [R.equals(0), R.always("12")],
-        [R.lt(R.__, 10), asTwoCharacterStr],
-        [R.T, R.toString],
-    ]),
+    noon2HighNoon,
+    formatAsString,
 )
 
 const toMinutesString = R.pipe(
     R.modulo(R.__, 60),
-    R.cond<number, string>([
-        [R.lt(R.__, 10), asTwoCharacterStr],
-        [R.T, R.toString],
-    ]),
+    formatAsString,
 )
 
-const customJoin = (x: string, y: string) => R.join(":", [x, y])
+const joinAsClockString = (x: string, y: string) => R.join(":", [x, y])
 
-const toClockString = R.converge(customJoin, [toHourString, toMinutesString])
+export const toClockString = R.converge(joinAsClockString, [
+    toHourString,
+    toMinutesString,
+])
 
 export const whatIsTheTime = R.pipe(
-    toTotalMinutes,
-    doMirrorTime,
-    toClockString,
+    validateInputString,
+    map(toTotalMinutes),
+    map(doMirroring),
+    map(toClockString),
+    maybe("Invalid Input String."),
 )
