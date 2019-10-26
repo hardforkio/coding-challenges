12383eae322426ea827a7cf6b92375811bdb1a24
kiliw <kilocheat@gmail.com>
2019-10-26 20:02:10 +0200

    Remove tslint and add eslint instead.

--- src/challenge.test.ts
+++ src/challenge.test.ts
@@ -1,7 +1,7 @@
 import test from "tape"
 import { add } from "./challenge"
 
-test("Should add two numbers", (assert) => {
+test("Should add two numbers", assert => {
   assert.plan(1)
   assert.equal(add(1, 2), 3)
 })
