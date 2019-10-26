2c98276cc60525fbce67bbfd3fdeaa1234aec51d
kiliw <kilocheat@gmail.com>
2019-10-26 08:56:37 +0200

    Refactoring.

--- src/challenge.test.ts
+++ src/challenge.test.ts
@@ -1,12 +1,32 @@
 import test from "tape"
 import { isValidWalk } from "./challenge"
 
-test("Should check for a valid walking time and end point", (assert) => {
-  assert.equal(isValidWalk(["n", "s", "n", "s", "n", "s", "n", "s", "n", "s"]), true, "is a valid walk")
-  assert.equal(isValidWalk(["w", "e", "w", "e", "w", "e", "w", "e", "w", "e", "w", "e"]), false, "walk is too long")
+test("Should check for a valid walking time and end point", assert => {
+  assert.equal(
+    isValidWalk(["n", "s", "n", "s", "n", "s", "n", "s", "n", "s"]),
+    true,
+    "is a valid walk"
+  )
+  assert.equal(
+    isValidWalk(["w", "e", "w", "e", "w", "e", "w", "e", "w", "e", "w", "e"]),
+    false,
+    "walk is too long"
+  )
   assert.equal(isValidWalk(["w"]), false, "walk is too short")
-  assert.equal(isValidWalk(["n", "n", "n", "s", "n", "s", "n", "s", "n", "s"]), false, "does not retun at home")
-  assert.equal(isValidWalk(["n", "n", "n", "s", "s", "s", "n", "n", "s", "s"]), true, "is valid")
-  assert.equal(isValidWalk(["n", "s", "e", "w", "w", "w", "e", "e", "n", "s"]), true, "is valid")
+  assert.equal(
+    isValidWalk(["n", "n", "n", "s", "n", "s", "n", "s", "n", "s"]),
+    false,
+    "does not retun at home"
+  )
+  assert.equal(
+    isValidWalk(["n", "n", "n", "s", "s", "s", "n", "n", "s", "s"]),
+    true,
+    "is valid"
+  )
+  assert.equal(
+    isValidWalk(["n", "s", "e", "w", "w", "w", "e", "e", "n", "s"]),
+    true,
+    "is valid"
+  )
   assert.end()
 })
--- src/challenge.ts
+++ src/challenge.ts
@@ -1,33 +1,37 @@
 // https://www.codewars.com/kata/54da539698b8a2ad76000228
-import * as R from 'ramda'
+import * as R from "ramda"
 
-export const isValidWalk = (walk: string[]): boolean => {
-    // insert brilliant code here
-    let vertical = 0
-    let horizontal = 0
-    if(R.length(walk) !== 10) {
-        return false
+const isNotTenMinutes = R.pipe<string[], number, boolean, boolean>(
+  R.length,
+  R.equals(10),
+  R.not
+)
+
+const returnedToStart = (walk: string[]) => {
+  let vertical = 0
+  let horizontal = 0
+  return R.reduce((isHome, direction) => {
+    if (direction === "n") {
+      vertical += 1
     }
-    const isHome = R.reduce((isHome, direction) => {
-        if(direction === "n"){
-            vertical += 1
-        }
-        if(direction === "s"){
-            vertical -= 1
-        }
-        if(direction === "e"){
-            horizontal += 1
-        }
-        if(direction === "w"){
-            horizontal -= 1
-        }
-        if (vertical === 0 && horizontal === 0) {
-            return true
-        }
-        return false
-    }, false)(walk)
-    if(isHome){
-        return true
+    if (direction === "s") {
+      vertical -= 1
+    }
+    if (direction === "e") {
+      horizontal += 1
+    }
+    if (direction === "w") {
+      horizontal -= 1
+    }
+    if (vertical === 0 && horizontal === 0) {
+      return true
     }
     return false
-  }
+  }, false)(walk)
+}
+
+export const isValidWalk: (walk: string[]) => boolean = R.cond([
+  [isNotTenMinutes, R.always(false)],
+  [returnedToStart, R.always(true)],
+  [R.T, R.always(false)]
+])
