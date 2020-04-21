8ccfed55bc462798eeabde12f7c8632e18267d71
Jakob J. Kolb <jakob.kolb@pik-potsdam.de>
2020-04-21 22:01:53 +0200

    refactored solution and added tests for functions

--- src/challenge.test.ts
+++ src/challenge.test.ts
@@ -1,20 +1,77 @@
 import test from 'tape'
-import { WhatIsTheTime } from './challenge'
+import {
+  whatIsTheTime,
+  hours,
+  minutes,
+  hoursString,
+  convertTimeStringToIntMinutes,
+  convertIntMinutesToTimeString,
+} from './challenge'
 
 test('Basic Test', assert => {
   assert.plan(1)
-  assert.equal(WhatIsTheTime('06:35'), '05:25')
+  assert.equal(whatIsTheTime('06:35'), '05:25')
 })
 
 test('Basic Test', assert => {
   assert.plan(1)
-  assert.equal(WhatIsTheTime('11:59'), '12:01')
+  assert.equal(whatIsTheTime('11:59'), '12:01')
 })
 test('Basic Test', assert => {
   assert.plan(1)
-  assert.equal(WhatIsTheTime('12:02'), '11:58')
+  assert.equal(whatIsTheTime('12:02'), '11:58')
 })
 test('Basic Test', assert => {
   assert.plan(1)
-  assert.equal(WhatIsTheTime('06:00'), '06:00')
+  assert.equal(whatIsTheTime('06:00'), '06:00')
+})
+
+test('Test hour string to int conversion 1', assert => {
+  assert.plan(1)
+  assert.equal(hours('12:25'), 0)
+})
+
+test('Test hour string to int conversion 2', assert => {
+  assert.plan(1)
+  assert.equal(hours('05:25'), 5)
+})
+
+test('Test minute string to int conversion 1', assert => {
+  assert.plan(1)
+  assert.equal(minutes('12:25'), 25)
+})
+
+test('Test minute string to int conversion 2', assert => {
+  assert.plan(1)
+  assert.equal(minutes('05:00'), 0)
+})
+
+test('Time String to Int conversion 1', assert => {
+  assert.plan(1)
+  assert.equal(convertTimeStringToIntMinutes('12:21'), 21)
+})
+
+test('Time String to Int conversion 1', assert => {
+  assert.plan(1)
+  assert.equal(convertTimeStringToIntMinutes('02:21'), 141)
+})
+
+test('Minutes to hour string 1', assert => {
+  assert.plan(1)
+  assert.equal(hoursString(21), '12')
+})
+
+test('Minutes to hour string 2', assert => {
+  assert.plan(1)
+  assert.equal(hoursString(121), '02')
+})
+
+test('Minutes to time string 1', assert => {
+  assert.plan(1)
+  assert.equal(convertIntMinutesToTimeString(21), '12:21')
+})
+
+test('Minutes to time string 2', assert => {
+  assert.plan(1)
+  assert.equal(convertIntMinutesToTimeString(361), '06:01')
 })
--- src/challenge.ts
+++ src/challenge.ts
@@ -1,37 +1,53 @@
-import { min } from 'ramda'
+export function hours(time: string): number {
+  let hourString = time.slice(0, 2)
+  if (time.slice(0, 2) === '12') {
+    hourString = '00'
+  }
+  return parseInt(hourString)
+}
 
+export function minutes(time: string): number {
+  return parseInt(time.slice(3, 5))
+}
+
+export function convertTimeStringToIntMinutes(time: string): number {
+  return 60 * hours(time) + minutes(time)
+}
+
+export function mirrorAngleOnVerticalAxis(halfdegrees: number): number {
+  return -halfdegrees + 12 * 60
+}
+
+export function minutesString(minutes: number): string {
+  return `0${minutes % 60}`.substr(-2)
+}
+
+export function hoursString(minutes: number): string {
+  const hours = `0${(minutes - (minutes % 60)) / 60}`.substr(-2)
+  if (hours === '00') {
+    return '12'
+  } else {
+    return hours
+  }
+}
+
+export function convertIntMinutesToTimeString(minutes: number): string {
+  const minutesbase60 = minutes % 60
+  let hours = (minutes - minutesbase60) / 60
+  if (hours === 0) {
+    hours = 12
+  }
+  const hourString = ('00' + `${hours}`).substr(-2)
+  const minuteString = ('00' + `${minutesbase60}`).substr(-2)
+  return hourString + ':' + minuteString
+}
 // Taken from https://www.codewars.com/kata/clock-in-mirror/javascript
-export function WhatIsTheTime(timeInMirror: string) {
+export function whatIsTheTime(timeInMirror: string): string {
   // "05:25" --> "06:35";
   // Happy Coding
 
-  const ConvertTimeStringToIntMinutes = function(time: string) {
-    let hourString: string = time.slice(0, 2)
-    const minuteString: string = time.slice(3, 5)
-    if (hourString === '12') {
-      hourString = '00'
-    }
-    const hourInt: number = parseInt(hourString)
-    const minuteInt: number = parseInt(minuteString)
-    return 60 * hourInt + minuteInt
-  }
-
-  const MirrorAngleOnVerticalAxis = (halfdegrees: number) =>
-    -halfdegrees + 12 * 60
-
-  const ConvertIntMinutesToTimeString = function(minutes: number) {
-    const minutesbase60 = minutes % 60
-    let hours = (minutes - minutesbase60) / 60
-    if (hours === 0) {
-      hours = 12
-    }
-    const hourString = ('00' + `${hours}`).substr(-2)
-    const minuteString = ('00' + `${minutesbase60}`).substr(-2)
-    return hourString + ':' + minuteString
-  }
-
-  const timeInt: number = ConvertTimeStringToIntMinutes(timeInMirror)
-  const mirroredInt: number = MirrorAngleOnVerticalAxis(timeInt)
-  const mirroredTime = ConvertIntMinutesToTimeString(mirroredInt)
+  const timeInt: number = convertTimeStringToIntMinutes(timeInMirror)
+  const mirroredInt: number = mirrorAngleOnVerticalAxis(timeInt)
+  const mirroredTime = convertIntMinutesToTimeString(mirroredInt)
   return mirroredTime
 }
