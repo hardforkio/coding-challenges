b7a156522ac08da0a1f9e0fbe51413fbd4926aea
Jakob J. Kolb <jakob.kolb@pik-potsdam.de>
2020-04-28 12:02:21 +0200

    propose solution 1

--- src/challenge.test.ts
+++ src/challenge.test.ts
@@ -1,8 +1,10 @@
-import { deepEqual } from './challenge'
+import { deepEqual, allTrue, compareElementwise } from './challenge'
 
 const array1 = [1, 2, 3, 4, 5]
 const array2 = [1, 2, 3, 4, 5]
 const array3 = [5, 4, 3, 2, 1]
+const array1xarray2 = [true, true, true, true, true]
+const array1xarray3 = [false, false, true, false, false]
 
 test('two arrays with the same content', () => {
   expect(deepEqual(array1, array2)).toEqual(true)
@@ -10,4 +12,20 @@ test('two arrays with the same content', () => {
 
 test('two arrays with different content', () => {
   expect(deepEqual(array1, array3)).toEqual(false)
+})
+
+test('two arrays with the same content', () => {
+  expect(compareElementwise(array1, array2)).toEqual(array1xarray2)
+})
+
+test('two arrays with different content', () => {
+  expect(compareElementwise(array1, array3)).toEqual(array1xarray3)
+})
+
+test('all true array', () => {
+  expect(allTrue(array1xarray2)).toEqual(true)
+})
+
+test('not all true array', () => {
+  expect(allTrue(array1xarray3)).toEqual(false)
 })
--- src/challenge.ts
+++ src/challenge.ts
@@ -1,6 +1,10 @@
-export const deepEqual: (
-  array1: number[],
-  array2: number[],
-) => boolean = () => {
-  return false
-}
+import { zipWith, equals, all, pipe } from 'ramda'
+
+export const compareElementwise = zipWith(equals)
+
+export const allTrue = all(equals(true))
+
+export const deepEqual = pipe(
+  compareElementwise,
+  allTrue,
+)
