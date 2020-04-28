eefc67270abcafc1c66da0cfee7455fe84075880
Jakob Kolb <kolb@hardfork.io>
2020-04-28 14:52:00 +0200

    Apply suggestions from code review
    Co-Authored-By: Kilian W <43745180+kiliw@users.noreply.github.com>

--- src/challenge.ts
+++ src/challenge.ts
@@ -3,19 +3,19 @@ import { zipWith, equals, all, pipe } from 'ramda'
 export const compareElementwise: (
   array1: number[],
   array2: number[],
-) => boolean[] = (array1, array2) => {
-  return zipWith(equals, array1, array2)
-}
+) => boolean[] = zipWith<number, number, boolean>(equals)
 
 export const allTrue = all(equals(true))
 
 export const deepEqual: (array1: number[], array2: number[]) => boolean = (
   array1,
   array2,
-) => {
-  const check = pipe(
-    compareElementwise,
-    allTrue,
-  )
-  return check(array1, array2)
-}
+) = pipe<
+  number[],
+  number[],
+  boolean[],
+  boolean
+>(
+  compareElementwise,
+  allTrue,
+)
