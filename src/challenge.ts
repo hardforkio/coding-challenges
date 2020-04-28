1e562844752edde344284f415ec1211387761c5d
Jakob J. Kolb <jakob.kolb@pik-potsdam.de>
2020-04-28 18:37:04 +0200

    set types for zipWidth to satisfy linter

--- src/challenge.ts
+++ src/challenge.ts
@@ -1,6 +1,6 @@
 import { zipWith, equals, all, pipe } from 'ramda'
 
-export const compareElementwise = zipWith(equals)
+export const compareElementwise = zipWith<number, number, boolean>(equals)
 
 export const allTrue = all(equals(true))
 
