74602f183fceeb08a42a48663ba5e0a5378bab18
Jakob J. Kolb <jakob.kolb@pik-potsdam.de>
2020-04-28 20:39:25 +0200

    propose solution to mumbling challenge

--- src/challenge.test.ts
+++ src/challenge.test.ts
@@ -1,13 +1,30 @@
-import { accum } from './challenge'
+import { accum, addSummand, createSummand } from './challenge'
 
-test('Should add two numbers', () => {
+test('Should return mumbled string', () => {
   expect(accum('ZpglnRxqenU')).toEqual(
     'Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu',
   )
 })
 
-test('Should add two numbers', () => {
+test('Should return mumbled string', () => {
   expect(accum('NyffsGeyylB')).toEqual(
     'N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb',
   )
+})
+
+test('Should add entry to list of mubled string sylables', () => {
+  expect(addSummand(['A', 'Bb', 'Ccc'], 'n')).toEqual([
+    'A',
+    'Bb',
+    'Ccc',
+    'Nnnn',
+  ])
+})
+
+test('Should return Xxxxx', () => {
+  expect(createSummand('X', 4)).toEqual('Xxxxx')
+})
+
+test('Should return Aaaa', () => {
+  expect(createSummand('a', 3)).toEqual('Aaaa')
 })
--- src/challenge.ts
+++ src/challenge.ts
@@ -1,5 +1,23 @@
 // https://www.codewars.com/kata/mumbling/javascript
 
+import { reduce } from 'ramda'
+
+export const createSummand: (letter: string, length: number) => string = (
+  letter,
+  length,
+) =>
+  `${letter.toUpperCase()}${Array(length)
+    .fill(letter.toLowerCase())
+    .join('')}`
+
+export const addSummand: (summands: string[], letter: string) => string[] = (
+  summands,
+  letter,
+) => {
+  summands.push(createSummand(letter, summands.length))
+  return summands
+}
+
 export function accum(input: string): string {
-  return ''
+  return reduce(addSummand, [], input.split('')).join('-')
 }
