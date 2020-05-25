a58393f75bcdb031c11aa7066e3921773bfff9d9
Jakob J. Kolb <kolb@hardfork.io>
2020-05-25 20:27:33 +0200

    propose solution for ten minute walk

--- src/challenge.test.ts
+++ src/challenge.test.ts
@@ -1,10 +1,74 @@
-import test from "tape"
-import { isValidWalk } from "./challenge"
+import test from 'tape'
+import * as C from './challenge'
 
-test("Should check for a valid walking time and end point", (assert) => {
-  assert.equal(isValidWalk(["n", "s", "n", "s", "n", "s", "n", "s", "n", "s"]), true, "is a valid walk")
-  assert.equal(isValidWalk(["w", "e", "w", "e", "w", "e", "w", "e", "w", "e", "w", "e"]), false, "walk is too long")
-  assert.equal(isValidWalk(["w"]), false, "walk is too short")
-  assert.equal(isValidWalk(["n", "n", "n", "s", "n", "s", "n", "s", "n", "s"]), false, "does not retun at home")
+test('Should check for a valid walking time and end point', assert => {
+  assert.equal(
+    C.isValidWalk(['n', 's', 'n', 's', 'n', 's', 'n', 's', 'n', 's']),
+    true,
+    'is a valid walk'
+  )
+  assert.equal(
+    C.isValidWalk(['w', 'e', 'w', 'e', 'w', 'e', 'w', 'e', 'w', 'e', 'w', 'e']),
+    false,
+    'walk is too long'
+  )
+  assert.equal(C.isValidWalk(['w']), false, 'walk is too short')
+  assert.equal(
+    C.isValidWalk(['n', 'n', 'n', 's', 'n', 's', 'n', 's', 'n', 's']),
+    false,
+    'does not retun at home'
+  )
+  assert.end()
+})
+
+test('test if walk is 10 steps i.e. 10 minutes long', assert => {
+  assert.equal(
+    C.isTenMinutesLong(['n', 'n', 'n', 'n', 'n', 'n', 'n', 'n', 'n', 'n']),
+    true,
+    'is ten steps long'
+  )
+  assert.equal(
+    C.isTenMinutesLong(['n', 'n', 'n', 'n', 'n']),
+    false,
+    'is shorter than 10 steps'
+  )
+  assert.equal(
+    C.isTenMinutesLong([
+      'n',
+      'n',
+      'n',
+      'n',
+      'n',
+      'n',
+      'n',
+      'n',
+      'n',
+      'n',
+      'n',
+      'n',
+      'n'
+    ]),
+    false,
+    'is more than ten steps long'
+  )
+  assert.end()
+})
+
+test('should check if walk returns to origin', assert => {
+  assert.equal(
+    C.returnsToOrigin(['n', 's', 'w', 'e']),
+    true,
+    'returns to origin'
+  )
+  assert.equal(
+    C.returnsToOrigin(['n', 'n', 'e', 's']),
+    false,
+    'does not return to origin'
+  )
+  assert.equal(
+    C.returnsToOrigin([]),
+    true,
+    'does not leave origin, therefore valid'
+  )
   assert.end()
 })
--- src/challenge.ts
+++ src/challenge.ts
@@ -1,6 +1,33 @@
 // https://www.codewars.com/kata/54da539698b8a2ad76000228
 
-export const isValidWalk = (walk: string[]): boolean => {
-    // insert brilliant code here
-    return false
+import R from 'ramda'
+
+type possition = { toNorth: number; toWest: number }
+
+const walkEvolver = (direction: string) => {
+  switch (direction) {
+    case 'n':
+      return R.evolve({ toNorth: R.add(1) })
+    case 's':
+      return R.evolve({ toNorth: R.add(-1) })
+    case 'w':
+      return R.evolve({ toWest: R.add(1) })
+    case 'e':
+      return R.evolve({ toWest: R.add(-1) })
+    default:
+      return R.evolve({})
   }
+}
+
+export const walkReducer = (acc: possition, curr: string) =>
+  walkEvolver(curr)(acc)
+
+export const returnsToOrigin = (walk: string[]): boolean => {
+  const finalPossition = R.reduce(walkReducer, { toNorth: 0, toWest: 0 }, walk)
+  return finalPossition.toNorth === 0 && finalPossition.toWest === 0
+}
+
+export const isTenMinutesLong = (walk: string[]): boolean => walk.length === 10
+
+export const isValidWalk = (walk: string[]): boolean =>
+  isTenMinutesLong(walk) ? returnsToOrigin(walk) : false
