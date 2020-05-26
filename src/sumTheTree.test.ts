8cab1fb5f5a10e3e76b5d7ec11ffcdd9e3e28c08
Jakob J. Kolb <kolb@hardfork.io>
2020-05-26 09:37:00 +0200

    fix linting issues

--- src/sumTheTree.test.ts
+++ src/sumTheTree.test.ts
@@ -1,20 +1,20 @@
-import { Node, sumTheTreeValues } from './sumTheTree'
+import * as S from './sumTheTree'
 
-const simpleNode: Node = {
+const simpleNode: S.Node = {
   left: { left: null, right: null, value: 1 },
   right: { left: null, right: null, value: 2 },
   value: 10,
 }
-const unbalancedNode: Node = {
+const unbalancedNode: S.Node = {
   left: { left: null, right: null, value: 0 },
   right: { left: null, right: { left: null, right: null, value: 1 }, value: 0 },
   value: 11,
 }
 
 test('Sums up all children', () => {
-  expect(sumTheTreeValues(simpleNode)).toEqual(13)
+  expect(S.sumTheTreeValues(simpleNode)).toEqual(13)
 })
 
 test('Handles unbalanced trees', () => {
-  expect(sumTheTreeValues(unbalancedNode)).toEqual(12)
+  expect(S.sumTheTreeValues(unbalancedNode)).toEqual(12)
 })
