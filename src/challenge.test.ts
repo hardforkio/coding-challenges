2b70495a55d7c50b9608735785d8f24ab23ba9c6
Jakob J. Kolb <kolb@hardfork.io>
2020-05-26 17:02:13 +0200

    add describe to tests to make the results more comprehensible

--- src/challenge.test.ts
+++ src/challenge.test.ts
@@ -10,36 +10,40 @@ const array2 = ['a', 'b', 'c']
 const array3 = ['a', 'b', 'a']
 const array1xarray2 = [true, true, true]
 const array1xarray3 = [true, true, false]
-
-test('two arrays with the same content', () => {
-  expect(allElementsEqual(array1, array2)).toEqual(true)
+describe('allElementsEqual', () => {
+  test('should return true for two arrays with the same content', () => {
+    expect(allElementsEqual(array1, array2)).toEqual(true)
+  })
+  test('should return false for two arrays with different content', () => {
+    expect(allElementsEqual(array1, array3)).toEqual(false)
+  })
+})
+describe('compareElementwise', () => {
+  test('should return all true array two arrays with the same content', () => {
+    expect(compareElementwise(array1, array2)).toEqual(array1xarray2)
+  })
+  test('should return array with true, except for false where input arrays differ', () => {
+    expect(compareElementwise(array1, array3)).toEqual(array1xarray3)
+  })
 })
 
-test('two arrays with different content', () => {
-  expect(allElementsEqual(array1, array3)).toEqual(false)
+describe('allTrue', () => {
+  test('should return true for all true array', () => {
+    expect(allTrue(array1xarray2)).toEqual(true)
+  })
+  test('should return false for not all true array', () => {
+    expect(allTrue(array1xarray3)).toEqual(false)
+  })
 })
 
-test('two arrays with the same content', () => {
-  expect(compareElementwise(array1, array2)).toEqual(array1xarray2)
-})
-
-test('two arrays with different content', () => {
-  expect(compareElementwise(array1, array3)).toEqual(array1xarray3)
-})
-
-test('all true array', () => {
-  expect(allTrue(array1xarray2)).toEqual(true)
-})
-
-test('not all true array', () => {
-  expect(allTrue(array1xarray3)).toEqual(false)
-})
-test('Expected to return true since racecar is a palindrome', () => {
-  expect(isPalindrom('racecar')).toEqual(true)
-})
-test('Expected to return true. Does it handle case as expected?', () => {
-  expect(isPalindrom('Anna')).toEqual(true)
-})
-test('Expected to return false since table is not a palindrome', () => {
-  expect(isPalindrom('table')).toEqual(false)
+describe('isPlaindrom', () => {
+  test('should return true since racecar is a palindrome', () => {
+    expect(isPalindrom('racecar')).toEqual(true)
+  })
+  test('should return true for Anna. Does it handle case as expected?', () => {
+    expect(isPalindrom('Anna')).toEqual(true)
+  })
+  test('should return false since table is not a palindrome', () => {
+    expect(isPalindrom('table')).toEqual(false)
+  })
 })
