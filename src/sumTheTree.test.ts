d7c6d60b8646a9fedfecd88babcd135886705168
Jakob J. Kolb <kolb@hardfork.io>
2020-05-28 17:23:13 +0200

    propose (mostly) pointfree solution with breadth first flattening of tree

--- src/sumTheTree.test.ts
+++ src/sumTheTree.test.ts
@@ -1,77 +1,152 @@
-import * as Context from './sumTheTree'
+import {
+  sumTheTreeValues,
+  getChildrenAsArray,
+  treeToArray,
+  Node,
+  addValueOfNodeToResultAndQueueChildren,
+  appenValueOfFirstNodeInQueToResultArray,
+  getChildrenOfFirstElementAsQueue,
+  dropFirstNodeFromQueAndAddItsChildrenToQueue,
+  wrapNodeInAccumulator,
+} from './sumTheTree'
 
-const simpleNode: Context.Node = {
+const simpleNode: Node = {
   left: null,
   right: null,
   value: 3,
 }
-const simpleNodeWithTwoChildren: Context.Node = {
+const simpleNodeWithTwoChildren: Node = {
   left: { left: null, right: null, value: 1 },
   right: { left: null, right: null, value: 2 },
   value: 10,
 }
 
-const simpleLeftleaningNode: Context.Node = {
+const simpleLeftleaningNode: Node = {
   left: { left: null, right: null, value: 1 },
   right: null,
   value: 10,
 }
 
-const simpleRightleaningNode: Context.Node = {
+const simpleRightleaningNode: Node = {
   left: null,
   right: { left: null, right: null, value: 2 },
   value: 10,
 }
 
-const unbalancedNode: Context.Node = {
+const unbalancedNode: Node = {
   left: { left: null, right: null, value: 0 },
   right: { left: null, right: { left: null, right: null, value: 1 }, value: 0 },
   value: 11,
 }
 describe('sumTheTreeValues', () => {
   test('should sum up all children', () => {
-    expect(Context.sumTheTreeValues(simpleNodeWithTwoChildren)).toEqual(13)
+    expect(sumTheTreeValues(simpleNodeWithTwoChildren)).toEqual(13)
   })
   test('should handle unbalanced trees', () => {
-    expect(Context.sumTheTreeValues(unbalancedNode)).toEqual(12)
+    expect(sumTheTreeValues(unbalancedNode)).toEqual(12)
   })
 })
 
 describe('getChildren', () => {
   test('should return empty list for node without children', () => {
-    expect(Context.getChildren({ left: null, right: null, value: 1 })).toEqual(
+    expect(getChildrenAsArray({ left: null, right: null, value: 1 })).toEqual(
       [],
     )
   })
   test('should return list of length one for node with one child on the left', () => {
     const leftNode = { left: null, right: null, value: 5 }
     expect(
-      Context.getChildren({ left: leftNode, right: null, value: 1 }),
+      getChildrenAsArray({ left: leftNode, right: null, value: 1 }),
     ).toEqual([leftNode])
   })
   test('should return list of length one for node with one child on the right', () => {
     const rightNode = { left: null, right: null, value: 5 }
     expect(
-      Context.getChildren({ left: null, right: rightNode, value: 1 }),
+      getChildrenAsArray({ left: null, right: rightNode, value: 1 }),
     ).toEqual([rightNode])
   })
   test('should return list of length two for node with two children', () => {
     const rightNode = { left: null, right: null, value: 5 }
     const leftNode = { left: null, right: null, value: 1 }
     expect(
-      Context.getChildren({ left: leftNode, right: rightNode, value: 1 }),
+      getChildrenAsArray({ left: leftNode, right: rightNode, value: 1 }),
     ).toEqual([leftNode, rightNode])
   })
 })
 
 describe('mapTreeToArray', () => {
-  test('should return list of length one for single node without children', () => {
-    expect(Context.mapTreeToArray(simpleNode)).toEqual([simpleNode.value])
-  })
+  test('should return list of length one for single node without children', () =>
+    expect(treeToArray(simpleNode)).toEqual([simpleNode.value]))
   test('should return list of length two for node with one child on left', () => {
-    expect(Context.mapTreeToArray(simpleLeftleaningNode)).toEqual([10, 1])
+    expect(treeToArray(simpleLeftleaningNode)).toEqual([10, 1])
   })
   test('should return list of length two for node with one child on right', () => {
-    expect(Context.mapTreeToArray(simpleRightleaningNode)).toEqual([10, 2])
+    expect(treeToArray(simpleRightleaningNode)).toEqual([10, 2])
+  })
+})
+
+describe('flattenTree', () => {
+  test('Should correctly add value to result array', () =>
+    expect(
+      appenValueOfFirstNodeInQueToResultArray({
+        result: [],
+        queue: [{ value: 5, left: null, right: null }],
+      }),
+    ).toEqual([5]))
+  test('should add node value to result array and not add further nodes to the queue', () =>
+    expect(
+      addValueOfNodeToResultAndQueueChildren({
+        result: [],
+        queue: [{ value: 5, left: null, right: null }],
+      }),
+    ).toEqual({ result: [5], queue: [] }))
+
+  describe('getChildrenOfFirstElementInQueueAsQueue', () => {
+    test('should get children of first node in queue and return them as queue', () => {
+      const firstNode = {
+        value: 0,
+        left: { value: 1, left: null, right: null },
+        right: { value: 2, left: null, right: null },
+      }
+      const secondNode = {
+        value: 3,
+        right: null,
+        left: null,
+      }
+      expect(
+        getChildrenOfFirstElementAsQueue({
+          result: [],
+          queue: [firstNode, secondNode],
+        }),
+      ).toEqual([firstNode.left, firstNode.right])
+    })
+  })
+  describe('dropFirstElementFromQueueAndAddItsChildrenAtEndOfQueue', () => {
+    test('should drop first node from queue and add its children at end of queue', () => {
+      const firstNode = {
+        value: 0,
+        left: { value: 1, left: null, right: null },
+        right: { value: 2, left: null, right: null },
+      }
+      const secondNode = {
+        value: 3,
+        right: null,
+        left: null,
+      }
+      expect(
+        dropFirstNodeFromQueAndAddItsChildrenToQueue({
+          result: [],
+          queue: [firstNode, secondNode],
+        }),
+      ).toEqual([secondNode, firstNode.left, firstNode.right])
+    })
+  })
+  describe('wrapNodeInAccumulator', () => {
+    test('should return accumulator with empty list as results and given node in queue', () => {
+      expect(wrapNodeInAccumulator(simpleNode)).toEqual({
+        result: [],
+        queue: [simpleNode],
+      })
+    })
   })
 })
--- src/sumTheTree.ts
+++ src/sumTheTree.ts
@@ -8,11 +8,14 @@ export type Node = {
   right: Node | null
 }
 
-type NodeList = ReadonlyArray<Node>
+type Queue = ReadonlyArray<Node>
 
-export type Accumulator = { value: number; unvisitedNodes: NodeList }
+export type Accumulator = {
+  result: ReadonlyArray<number>
+  queue: Queue
+}
 
-export const getChildren: (node: Node) => NodeList = R.pipe<
+export const getChildrenAsArray: (node: Node) => Queue = R.pipe<
   Node,
   ReadonlyArray<Node | null>,
   ReadonlyArray<Node>
@@ -21,25 +24,75 @@ export const getChildren: (node: Node) => NodeList = R.pipe<
   R.reject<any>(R.equals(null)),
 )
 
-export const treeReducer = (
-  acc: ReadonlyArray<number>,
-  currentElement: Node | null,
-): ReadonlyArray<number> => {
-  if (currentElement) {
-    return R.concat(
-      R.append(R.prop('value')(currentElement), acc),
-      R.reduce(treeReducer, [], getChildren(currentElement)),
-    )
-  }
-  return acc
-}
+const getFirstValueOfQueue: (accumulator: Accumulator) => number = R.pipe<
+  Accumulator,
+  Queue,
+  Node,
+  number
+>(
+  R.prop('queue'),
+  R.head,
+  R.prop('value'),
+)
 
-export const mapTreeToArray: (root: Node) => ReadonlyArray<number> = R.pipe(
-  (x: Node): NodeList => [x],
-  R.reduce(treeReducer, []),
+export const dropFirstElementFromQueue: (
+  accumulator: Accumulator,
+) => Queue = R.pipe(
+  R.prop('queue'),
+  R.drop(1),
+)
+
+export const getChildrenOfFirstElementAsQueue: (
+  accumulator: Accumulator,
+) => Queue = R.pipe(
+  R.prop('queue'),
+  R.take(1),
+  R.apply(getChildrenAsArray),
+)
+
+export const dropFirstNodeFromQueAndAddItsChildrenToQueue: (
+  accumulator: Accumulator,
+) => Queue = R.converge(R.concat, [
+  dropFirstElementFromQueue,
+  getChildrenOfFirstElementAsQueue,
+])
+
+export const appenValueOfFirstNodeInQueToResultArray: (
+  accumulator: Accumulator,
+) => ReadonlyArray<number> = R.converge(R.append, [
+  getFirstValueOfQueue,
+  R.prop('result'),
+])
+
+export const addValueOfNodeToResultAndQueueChildren: (
+  accumulator: Accumulator,
+) => Accumulator = R.applySpec({
+  result: appenValueOfFirstNodeInQueToResultArray,
+  queue: dropFirstNodeFromQueAndAddItsChildrenToQueue,
+})
+
+const queIsEmpty: (accumulator: Accumulator) => boolean = R.pipe(
+  R.prop('queue'),
+  R.prop('length'),
+  R.equals(0),
+)
+
+export const treeToArrayWithAccumulators: (
+  accumulator: Accumulator,
+) => Accumulator = R.until(queIsEmpty, addValueOfNodeToResultAndQueueChildren)
+
+export const wrapNodeInAccumulator: (node: Node) => Accumulator = R.applySpec({
+  result: () => [],
+  queue: (x: Node): Queue => [x],
+})
+
+export const treeToArray: (root: Node) => ReadonlyArray<number> = R.pipe(
+  wrapNodeInAccumulator,
+  treeToArrayWithAccumulators,
+  R.prop('result'),
 )
 
 export const sumTheTreeValues: (root: Node) => number = R.pipe(
-  mapTreeToArray,
+  treeToArray,
   R.sum,
 )
