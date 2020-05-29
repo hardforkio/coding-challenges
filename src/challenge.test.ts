6c3c0edaad75950f31467b9555eb26e8b5f1f4de
Jakob J. Kolb <kolb@hardfork.io>
2020-05-29 18:33:18 +0200

    make it pointfree and ugly af

--- src/challenge.test.ts
+++ src/challenge.test.ts
@@ -1,29 +1,110 @@
-import * as C from './challenge'
+import {
+  mumble,
+  repeatLetter1TimesAndNMinusOneTimesLowerCase,
+  allLettersMumbled,
+  takeLetterFromOriginalStringAndAddMumbledSylable,
+  extractFirstLetterOfOriginalAndSylableListLength,
+  upperOfLetter,
+  repeatLetterNTimesLower,
+  makeSylable
+} from './challenge'
 
-test('Should return mumbled string', () => {
-  expect(C.accum('ZpglnRxqenU')).toEqual(
-    'Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu'
-  )
+describe('mumble', () => {
+  test('Should return mumbled string', () => {
+    expect(mumble('ZpglnRxqenU')).toEqual(
+      'Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu'
+    )
+  })
+
+  test('Should return mumbled string', () => {
+    expect(mumble('NyffsGeyylB')).toEqual(
+      'N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb'
+    )
+  })
 })
 
-test('Should return mumbled string', () => {
-  expect(C.accum('NyffsGeyylB')).toEqual(
-    'N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb'
-  )
+describe('repeatLetter1TimesAndNMinuesOneTimesLowerCase', () => {
+  test('Should return Xxxxx given [x, 4]', () => {
+    expect(repeatLetter1TimesAndNMinusOneTimesLowerCase(['x', 4])).toEqual(
+      'Xxxxx'
+    )
+  })
+
+  test('Should return Aaaa given [x, 3]', () => {
+    expect(repeatLetter1TimesAndNMinusOneTimesLowerCase(['a', 3])).toEqual(
+      'Aaaa'
+    )
+  })
+  test('Should return B given [b, 0]', () => {
+    expect(repeatLetter1TimesAndNMinusOneTimesLowerCase(['b', 0])).toEqual('B')
+  })
+
+  describe('upperOfLetter', () => {
+    test('should take letter in pos 0 and return uppercase', () => {
+      expect(upperOfLetter(['a', 5])).toEqual('A')
+    })
+  })
+  describe('repeatLetterNTimesLower', () => {
+    test('should repeat letter in pos 0 n times with n in pos 1', () => {
+      expect(repeatLetterNTimesLower(['a', 5])).toEqual('aaaaa')
+    })
+  })
 })
 
-test('Should add entry to list of mubled string sylables', () => {
-  expect(
-    C.createSubstringFromLetterAndAddToList(['A', 'Bb', 'Ccc'], 'N')
-  ).toEqual(['A', 'Bb', 'Ccc', 'Nnnn'])
+describe('allLetterMumbled', () => {
+  test('should return true, if original string is empty', () => {
+    expect(allLettersMumbled({ original: [], sylables: [] })).toEqual(true)
+  })
+  test('should return true, if original string contains letters', () => {
+    expect(allLettersMumbled({ original: ['a'], sylables: [] })).toEqual(false)
+  })
 })
 
-test('Should return Xxxxx', () => {
-  expect(C.repeatLetter1TimesAndNMinusOneTimesLowerCase('X', 4)).toEqual(
-    'Xxxxx'
-  )
+describe('takeLetterFromOriginalStringAndAddMumbledSylable', () => {
+  test('should remove letter from original and add mumbled sylable to sylables', () => {
+    expect(
+      takeLetterFromOriginalStringAndAddMumbledSylable({
+        original: ['a'],
+        sylables: ['x']
+      })
+    ).toEqual({ original: [], sylables: ['x', 'Aa'] })
+  })
+  test('should remove one letter from original and add mumbled sylable to sylables', () => {
+    expect(
+      takeLetterFromOriginalStringAndAddMumbledSylable({
+        original: ['a', 'b'],
+        sylables: ['x']
+      })
+    ).toEqual({ original: ['b'], sylables: ['x', 'Aa'] })
+  })
 })
 
-test('Should return Aaaa', () => {
-  expect(C.repeatLetter1TimesAndNMinusOneTimesLowerCase('A', 3)).toEqual('Aaaa')
+describe('extractFirstLetterOfOriginalAndSylableListLength', () => {
+  test('should return first letter of original list and length of sylable list', () => {
+    expect(
+      extractFirstLetterOfOriginalAndSylableListLength({
+        original: ['a', 'b'],
+        sylables: ['x']
+      })
+    ).toEqual(['a', 1])
+  })
+  test('should return length zero for empty sylable list', () => {
+    expect(
+      extractFirstLetterOfOriginalAndSylableListLength({
+        original: ['a', 'b'],
+        sylables: []
+      })
+    ).toEqual(['a', 0])
+  })
+})
+
+describe('makeSylable', () => {
+  test('should create sylable according to mubledString state', () => {
+    expect(
+      makeSylable({
+        original: ['a', 'b'],
+        sylables: ['x']
+      })
+    ).toEqual('Aa')
+  })
 })
--- src/challenge.ts
+++ src/challenge.ts
@@ -1,23 +1,112 @@
 // https://www.codewars.com/kata/mumbling/javascript
 
 import R from 'ramda'
+import identical from 'ramda/es/identical'
 
-export const repeatLetter1TimesAndNMinusOneTimesLowerCase = (
-  letter: string,
-  n: number
-): string =>
-  R.join('', R.concat([R.toUpper(letter)], R.repeat(R.toLower(letter), n)))
+type mumbledStringList = {
+  original: string[]
+  sylables: string[]
+}
 
-export const createSubstringFromLetterAndAddToList = (
-  subStringList: string[],
-  letter: string
-): string[] =>
-  R.concat(subStringList, [
-    repeatLetter1TimesAndNMinusOneTimesLowerCase(letter, subStringList.length)
-  ])
+type LetterAndLength = (string | number)[]
 
-export const accum = R.pipe(
-  R.split(''),
-  R.reduce(createSubstringFromLetterAndAddToList, []),
+export const upperOfLetter = R.pipe<any, any, any, any>(
+  R.take(1),
+  R.apply(R.identity),
+  R.toUpper
+)
+
+export const repeatLetterNTimesLower = R.pipe<any, any, any>(
+  R.apply(R.repeat),
+  R.join('')
+)
+
+export const repeatLetter1TimesAndNMinusOneTimesLowerCase: (
+  [letter, n]: any
+) => string = R.pipe<any, any, any>(
+  R.juxt([upperOfLetter, repeatLetterNTimesLower]),
+  R.join('')
+)
+
+export const extractFirstLetterOfOriginalAndSylableListLength: (
+  m: mumbledStringList
+) => LetterAndLength = R.juxt<mumbledStringList, string | number>([
+  R.pipe<mumbledStringList, string[], string[], string, string>(
+    R.prop('original'),
+    R.take(1),
+    R.apply<string, string>(R.identity),
+    R.toLower
+  ),
+  R.pipe<mumbledStringList, string[], number>(
+    R.prop('sylables'),
+    R.prop('length')
+  )
+])
+
+export const makeSylable: (m: mumbledStringList) => string = R.pipe<
+  mumbledStringList,
+  LetterAndLength,
+  string
+>(
+  extractFirstLetterOfOriginalAndSylableListLength,
+  repeatLetter1TimesAndNMinusOneTimesLowerCase
+)
+
+export const addSylable = R.converge(R.concat, [
+  R.prop('sylables'),
+  R.pipe<mumbledStringList, string, string[]>(
+    makeSylable,
+    R.unapply(R.identity)
+  )
+])
+
+const dropOneFromOriginalList = R.pipe<mumbledStringList, string[], string[]>(
+  R.prop('original'),
+  R.drop(1)
+)
+
+export const takeLetterFromOriginalStringAndAddMumbledSylable: (
+  x0: mumbledStringList
+) => mumbledStringList = R.applySpec({
+  original: dropOneFromOriginalList,
+  sylables: addSylable
+})
+
+export const allLettersMumbled = R.pipe<
+  mumbledStringList,
+  string[],
+  number,
+  boolean
+>(
+  R.prop('original'),
+  R.prop('length'),
+  R.equals(0)
+)
+
+const joinSylables = R.pipe<mumbledStringList, string[], string>(
+  R.prop('sylables'),
   R.join('-')
+)
+export const makeSylables: (
+  x0: mumbledStringList
+) => mumbledStringList = R.until<mumbledStringList, mumbledStringList>(
+  allLettersMumbled,
+  takeLetterFromOriginalStringAndAddMumbledSylable
+)
+
+export const makeMumbledStringInProgress = (
+  original: string
+): mumbledStringList => ({
+  original: R.split('', original),
+  sylables: []
+})
+export const mumble = R.pipe<
+  string,
+  mumbledStringList,
+  mumbledStringList,
+  string
+>(
+  makeMumbledStringInProgress,
+  makeSylables,
+  joinSylables
 )
